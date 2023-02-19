be95b722ca6002df45c1af4eb3b7d4f1e1adb36f
MohamedHamed <MohamedHamed205>
2023-02-19 17:58:38 +0200

    model no 14

--- buliding_the_first_model.py
+++ buliding_the_first_model.py
@@ -26,6 +26,8 @@ model no.12  => [batch size  = 124, epoch = 150] 5conv layers with 224 filters +
                 lr=0.005, momentum=0.9, decay=0.01
 model no.13  => [batch size  = 124, epoch = 100] 5conv layers with 224 filters +testing different optimizer(from adam to sgd)
                 lr=0.05, momentum=0.9, decay=0.01
+model no.14  => [batch size  = 124, epoch = 200] 5conv layers with 224 filters +testing different optimizer(from adam to sgd)
+                lr=0.05, momentum=0.9, decay=0.01
 
 
 ****************************************************************'''
@@ -93,7 +95,7 @@ opt = SGD(lr=0.05, momentum=0.9, decay=0.01)
 
 #compiling
 model.compile(loss='binary_crossentropy',optimizer=opt,metrics=[tf.keras.metrics.Precision(), tf.keras.metrics.Recall(), "acc"])
-epochs = 100
+epochs = 200
 batch_size = 124
 
 #fitting
@@ -104,7 +106,7 @@ with tf.device("/GPU:0"):
         print(e)
 
 #saving
-model.save('./training/model13.h5')
+model.save('./training/model14.h5')
 
 
 #------------------PLOTTING-----------------------------------------------------------
